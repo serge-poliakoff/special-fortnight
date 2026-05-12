b9ac577933324ab90405730e59e866cee5fa63c6
Serge-Poliakoff <poliakob.serega@gmail.com>
2026-05-12 20:46:32 +0000

    Reformat test.sh

--- test.sh
+++ test.sh
@@ -1,42 +1,39 @@
-suc_c=0
-all=0
+total_succ=0
+total=0
 
-echo "Checking for correct tests..."
-for f in ./tests/correct/*; do
-    echo $f :
-    ./bin/tpcas --dry-run < $f
-    res=$?
-    if [ $res -eq 0 ]
-    then
-        suc_c=$(($suc_c + 1))
-    fi
-    all=$(($all + 1))
-done
+score_tests() {
+    local folder="$1"
+    local expected="$2"
+    local label="$3"
+    local suc_c=0
+    local all=0
+
+    for f in "$folder"/*; do
+        if [ -f "$f" ]; then
+            echo "$f :"
+            ./bin/tpcas --dry-run < "$f" 1>/dev/null
+            res=$?
+            if [ "$res" -eq "$expected" ]; then
+                suc_c=$((suc_c + 1))
+            fi
+            all=$((all + 1))
+        fi
+    done
+
+    echo "$label"
+    echo "$suc_c / $all tests passed successfully"
+    echo
+    
+    total_succ=$((total_succ + suc_c))
+    total=$((total + all))
+}
+
+score_tests "./tests/correct" 0 "Correct tests score:"
+
+score_tests "./tests/incorrect" 1 "Gramatically incorrect tests score:"
+
+score_tests "./tests/sem_inc" 2 "Sematically incorrect tests score:"
 
 echo
-echo "Checking on  gramtically incorrect tests..."
-for f in ./tests/incorrect/*; do
-    echo $f :
-    ./bin/tpcas --dry-run < $f
-    res=$?
-    if [ $res -eq 1 ]
-    then
-        suc_c=$(($suc_c + 1))
-    fi
-    all=$(($all + 1))
-done
-
-echo "Checking on semantically incorrect tests..."
-for f in ./tests/sem_inc/*; do
-    echo $f :
-    ./bin/tpcas --dry-run < $f
-    res=$?
-    if [ $res -eq 2 ]
-    then
-        suc_c=$(($suc_c + 1))
-    fi
-    all=$(($all + 1))
-done
-#todo: separate scores for each group
-echo
-echo $suc_c / $all tests passed successfully
+echo "Total score:"
+echo $total_succ / $total tests passed successfully
